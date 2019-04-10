7b5e3298bd593ee2b0ddb4ba551bccb7f038b541
Christopher Riffle <criffle@Christophers-Mac-Pro.local>
2019-04-10 12:07:56 -0400

    Added jquery slidetoggle to menu.  added blur event to close article

--- Article/Article.js
+++ Article/Article.js
@@ -1,24 +1,40 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
+
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = 'article-open';
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.textContent = "expand";
     
+    this.domElement.setAttribute('tabindex', '0');
+     
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
+    this.domElement.addEventListener('blur', this.blurArticle.bind(this));
     this.domElement.addEventListener('click', this.expandArticle.bind(this));
+
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle(this.expandButton);
+    this.domElement.classList.toggle('article-open');
+   
+  }
+
+  blurArticle(){
+
+    if (this.domElement.classList.contains('article-open'))
+      this.domElement.classList.toggle('article-open');
+
+    console.log("blur");
   }
 }
-
-/* START HERE: 
+ 
+/* START HERE: s
 
 - Select all classes named ".article" and assign that value to the articles variable.  
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,8 +1,11 @@
 
+
+
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu reference. 
-  console.log(menu);
-    menu.classList.toggle('menu--open')
+  
+    //menu.classList.toggle('menu--open')
+    $(".menu").slideToggle();
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -11,3 +14,5 @@ const menu = document.querySelector(".menu");
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', toggleMenu);
+
+ 
