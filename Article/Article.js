c40ab767df01a5ddc4ff3017a177cd2b60278f90
Christopher Riffle <criffle@Christophers-Mac-Pro.local>
2019-04-10 11:11:29 -0400

    Fixed bug in Article constructor added toggle code

--- Article/Article.js
+++ Article/Article.js
@@ -9,12 +9,12 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.domElement('click', this.expandArticle.bind(this));
+    this.domElement.addEventListener('click', this.expandArticle.bind(this));
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    
+    this.domElement.classList.toggle(this.expandButton);
   }
 }
 
